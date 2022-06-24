5b0c19a39feafc865f202ba2213bf59b08ebafaf
iamrasel <mdraselhossain2312@gmail.com>
2022-06-24 18:47:51 +0600

    Copy button in Delete Task screen

--- app/src/main/java/com/tjm/crushr/crushrDeleteDialog.java
+++ app/src/main/java/com/tjm/crushr/crushrDeleteDialog.java
@@ -2,9 +2,11 @@ package com.tjm.crushr;
 
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.ComponentName;
 import android.os.Bundle;
-import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Window;
 import android.widget.TextView;
 
@@ -21,13 +23,22 @@ public class crushrDeleteDialog extends Activity {
         setContentView(R.layout.crushr_delete_dialog);
 
         int width = (int)(getResources().getDisplayMetrics().widthPixels*0.80);
-        int height = (int)(getResources().getDisplayMetrics().heightPixels*0.30);
+        int height = (int)(getResources().getDisplayMetrics().heightPixels*0.40);
         getWindow().setLayout(width, height);
 
         final String task = getIntent().getExtras().getString(crushrProvider.EXTRA_WORD);
         final int appWidgetId = getIntent().getExtras().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID);
 
-        ((TextView)findViewById(R.id.message)).setText(Html.fromHtml(getString(R.string.delete_task, task)));
+        TextView message = (TextView) findViewById(R.id.message);
+        message.setMovementMethod(new ScrollingMovementMethod());
+        message.setText(task);
+
+        findViewById(R.id.copy_btn).setOnClickListener(v -> {
+            ClipboardManager clipBoard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            String copyTask = message.getText().toString();
+            ClipData clipData = ClipData.newPlainText("copyTask", copyTask);
+            clipBoard.setPrimaryClip(clipData);
+        });
 
         findViewById(R.id.input_cancel).setOnClickListener(view -> finish());
 
